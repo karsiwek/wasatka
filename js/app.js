34e32f75bf783fbfc89b4b334e4e17a82c7da596
karsiwek <karsiwek@gmail.com>
2016-06-26 20:37:50 +0200

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,85 @@
+'use strict';
+
+// Declare app level module which depends on views, and components
+angular.module('myApp', [
+  'ngRoute'
+]).
+config(['$routeProvider', function($routeProvider) {
+  $routeProvider.otherwise({redirectTo: '/view1'});
+}])
+.controller('graphCtrl', ['$scope', function($scope) {
+    $scope.arcs = window.arcs;
+    $scope.imageSize = window.size;
+    $scope.boxSize = boxSize;
+    $scope.whiskerSize = whiskerSize;
+    $scope.rotation = rotation;
+    $scope.$watch("arcs", function(){
+        console.log(1);
+    })
+
+    $scope.update = function(){
+        window.size=$scope.imageSize;
+        window.whiskerSize=$scope.whiskerSize;
+        window.boxSize=$scope.boxSize;
+        window.rotation=$scope.rotation;
+        window.reload();
+
+        setTimeout(function(){
+          $('input[name="daterange"]').daterangepicker({
+            locale: {
+              format: 'DD/MM'
+            },
+          });
+        },1);
+        $('input[name="daterange"]').attr("name", "range");
+    }
+
+    $scope.updateDay = function(arc){
+        arc.to = getToArc(arc.daterange);
+        arc.from = getFromArc(arc.daterange);
+
+        if(arc.isWhiskers){
+          arc.toWhirker = getToArc(arc.whiskers);
+          arc.fromWhirker = getFromArc(arc.whiskers);
+
+        }
+        $scope.update();
+    }
+
+    var getFromArc = function(daterange){
+      if(!daterange) return;
+      return daterange.split("-")[0].split("/")[1]*1-1+(daterange.split("-")[0].split("/")[0]*1)/30
+    }
+
+    var getToArc = function(daterange){
+      if(!daterange) return;
+      return daterange.split("-")[1].split("/")[1]*1-1+(daterange.split("-")[1].split("/")[0]*1)/30
+    }
+
+    $scope.addArc = function(){
+        var arc = {
+            colour : "#"+Math.floor(Math.random()*1000000),
+            offset : Math.floor(Math.random()*20)-10,
+            isWhiskers : true
+        }
+        arc.fromMonth = Math.floor(arc.from)+1;
+        arc.toMonth = Math.floor(arc.to)+1;
+        arc.fromDay = Math.floor((arc.from%1)*30)+1;
+        arc.toDay = Math.floor((arc.to%1)*30)+1;
+
+        $scope.arcs[$scope.arcs.length] = arc;
+        $scope.updateDay(arc);
+    }
+
+    $scope.remove = function(arc){
+        if(window.confirm("czy na pewno Pani Ilonko chcesz usunąć tę kreskę?")){
+            var elem = $scope.arcs.indexOf(arc);
+            if(elem>-1){
+                $scope.arcs.splice(elem,1);
+            }
+        }
+        $scope.update();
+    }
+
+
+}]);
